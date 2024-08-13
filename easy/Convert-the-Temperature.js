7908bbd65f9cecf5578c8aee80212369ec40c628
Fitsumhelina <permin1501424@gmail.com>
2024-08-13 13:02:05 +0300

    the first task complated on 8/11/2024

--- easy/Convert-the-Temperature.js
+++ easy/Convert-the-Temperature.js
@@ -1,3 +1,18 @@
+/* Question
+You are given a non-negative floating point number rounded to two decimal places celsius, that denotes the temperature in Celsius.
+
+You should convert Celsius into Kelvin and Fahrenheit and return it as an array ans = [kelvin, fahrenheit].
+
+Return the array ans. Answers within 10-5 of the actual answer will be accepted.
+
+Note that:
+
+Kelvin = Celsius + 273.15
+Fahrenheit = Celsius * 1.80 + 32.00 */
+
+// solution
+
+
 function convertTemperature(c) {
     const k = c + 273.15;
     const f = c * 1.8 + 32.0;
