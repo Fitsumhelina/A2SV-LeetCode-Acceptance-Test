db961d9af11ebcefe87e4efe7a8813d20ad2fdf3
Fitsumhelina <permin1501424@gmail.com>
2024-08-19 15:29:49 +0300

    the 16th task complated on 8/19/2024

---
+++ easy/moves-zero.js
@@ -0,0 +1,17 @@
+/*Question 
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array */
+
+//Solution
+
+function moveZeroes(nums) {
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i]!== 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            j++;
+        }
+    }
+}
+
